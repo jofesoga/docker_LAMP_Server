1b44306eebd4103a80e0dd206570da4d3452ac77
jofesoga <jofesoga@gmail.com>
2025-09-01 10:30:01 -0600

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -55,8 +55,6 @@ fi
 # Set PHP timezone
 /bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
 
-# Run Postfix
-/usr/sbin/postfix start
 
 # Run MariaDB
 /usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&
